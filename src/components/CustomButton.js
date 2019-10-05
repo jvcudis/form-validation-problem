1ff4896d481a15acb488a422dc470628832f619f
Ann Cudis <ann.cudis@xero.com>
2019-10-06 12:55:54 +1300

    Create CustomButton component

---
+++ src/components/CustomButton.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  hiddenInput: {
+    display: 'none'
+  }
+}))
+
+export default function CustomButton({
+  name = 'submit-button',
+  label = '',
+  type = 'submit',
+  color = 'primary'
+}) {
+  const classes = useStyles()
+  return (
+    <>
+      <input type={type} id={name}  className={classes.hiddenInput} />
+      <label htmlFor={name}>
+        <Button variant='contained' color={color} component='span'>
+          {label}
+        </Button>
+      </label>
+    </>
+  )
+}
