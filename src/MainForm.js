dc9e3a296e7a845163d91818c9d533589b201f11
Ann Cudis <ann.cudis@xero.com>
2019-10-06 12:05:18 +1300

    Use react-hook-form for inputs

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useForm from 'react-hook-form';
 
 export default function MainForm() {
-    return (
-      <>
-        MainForm here
-      </>
-    )
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = data => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name='email' ref={register({ required: true })} />
+      {errors.email && <span>This field is required</span>}
+      <input name='password' ref={register({ required: true })} />
+      {errors.password && <span>This field is required</span>}
+      <input type='submit' />
+    </form>
+  )
 }
