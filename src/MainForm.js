aa2ead4147c11161ffad116179f4ba2c6d1414de
Ann Cudis <ann.cudis@xero.com>
2019-10-06 12:15:58 +1300

    Use material ui TextField for input

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import useForm from 'react-hook-form';
+import React from 'react'
+import useForm from 'react-hook-form'
+import { TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginBottom: theme.spacing(2),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  }
+}))
 
 export default function MainForm() {
+  const classes = useStyles()
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => {
     console.log(data)
@@ -9,10 +20,36 @@ export default function MainForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input name='email' ref={register({ required: true })} />
-      {errors.email && <span>This field is required</span>}
-      <input name='password' ref={register({ required: true })} />
-      {errors.password && <span>This field is required</span>}
+      <TextField
+        id='email'
+        label='Email'
+        name='email'
+        className={classes.textField}
+        type='text'
+        fullWidth
+        error={errors.email !== undefined}
+        inputRef={register({ required: true })}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        helperText={errors.email && 'This field is required'}
+        variant='outlined'
+      />
+      <TextField
+        id='password'
+        label='Password'
+        name='password'
+        className={classes.textField}
+        type='password'
+        fullWidth
+        error={errors.password !== undefined}
+        inputRef={register({ required: true })}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        helperText={errors.password && 'This field is required'}
+        variant='outlined'
+      />
       <input type='submit' />
     </form>
   )
