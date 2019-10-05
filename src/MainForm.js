a86b831367243fcd77c543ac7a8450ffd979a457
Ann Cudis <ann.cudis@xero.com>
2019-10-06 12:23:08 +1300

    Use CustomInput for email and password

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import useForm from 'react-hook-form'
-import { TextField } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles(theme => ({
-  textField: {
-    marginBottom: theme.spacing(2),
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
-  }
-}))
+import CustomInput from './components/CustomInput'
 
 export default function MainForm() {
-  const classes = useStyles()
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => {
     console.log(data)
@@ -20,35 +10,19 @@ export default function MainForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        id='email'
+      <CustomInput
         label='Email'
         name='email'
-        className={classes.textField}
         type='text'
-        fullWidth
-        error={errors.email !== undefined}
-        inputRef={register({ required: true })}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        helperText={errors.email && 'This field is required'}
-        variant='outlined'
+        errors={errors}
+        register={register({ required: true })}
       />
-      <TextField
-        id='password'
+      <CustomInput
         label='Password'
         name='password'
-        className={classes.textField}
         type='password'
-        fullWidth
-        error={errors.password !== undefined}
-        inputRef={register({ required: true })}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        helperText={errors.password && 'This field is required'}
-        variant='outlined'
+        errors={errors}
+        register={register({ required: true })}
       />
       <input type='submit' />
     </form>
