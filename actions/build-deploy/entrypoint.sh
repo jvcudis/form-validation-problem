676531d1bf453bd0443844d737105a8dd324f25b
Ann Cudis <ann.cudis@xero.com>
2019-10-06 19:38:25 +1300

    Add user and email env

--- actions/build-deploy/entrypoint.sh
+++ actions/build-deploy/entrypoint.sh
@@ -7,6 +7,6 @@ npm install gh-pages --global && \
 gh-pages -r "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" \
          -b "gh-pages2" \
          -m "Update page content" \
-         -u "${GITHUB_ACTOR}" \
+         -u "${GITHUB_USER} <${GITHUB_EMAIL}>" \
          -d build \
          -x
