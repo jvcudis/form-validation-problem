0b1dde482230b2fbf22148765d1b8d004a67c454
Ann Cudis <ann.cudis@xero.com>
2019-10-06 16:34:10 +1300

    Remove the semicolons

--- src/__tests__/MainForm.test.js
+++ src/__tests__/MainForm.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import MainForm from '../MainForm';
+import React from 'react'
+import { shallow } from 'enzyme'
+import MainForm from '../MainForm'
 
 it('renders without crashing', () => {
-  shallow(<MainForm />);
-});
+  shallow(<MainForm />)
+})
