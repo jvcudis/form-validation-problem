8f2f4c64d69b5ec056d2e92c50a7eadc8d359cc3
Ann Cudis <ann.cudis@xero.com>
2019-10-06 18:48:02 +1300

    Fix deploy error

--- actions/build-deploy/entrypoint.sh
+++ actions/build-deploy/entrypoint.sh
@@ -2,8 +2,5 @@
 
 export CI=true
 
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
 npm ci
 npm run deploy
--- actions/execute-test/entrypoint.sh
+++ actions/execute-test/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-export CI=true
-
 npm ci
 CI=true npm test
