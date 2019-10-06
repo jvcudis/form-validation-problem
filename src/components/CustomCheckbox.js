2f6496d9cad9784ad0edd598043bbbdaa3ee6d57
Ann Cudis <ann.cudis@xero.com>
2019-10-06 15:27:00 +1300

    Add CustomCheckbox component

---
+++ src/components/CustomCheckbox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FormControlLabel, Checkbox } from '@material-ui/core'
+
+export default function CustomCheckbox({
+  name,
+  value,
+  label,
+  handleChange
+}) {
+  return (
+    <FormControlLabel
+      control={<Checkbox onChange={handleChange(name)} value={value} />}
+      label={label}
+    />
+  )
+}
