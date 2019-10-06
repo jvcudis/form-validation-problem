0d7ff3ebfe731343d60116679354893d5402263e
Ann Cudis <ann.cudis@xero.com>
2019-10-06 18:35:03 +1300

    Add job dependency and fix script

--- actions/build-deploy/entrypoint.sh
+++ actions/build-deploy/entrypoint.sh
@@ -3,7 +3,7 @@
 export CI=true
 
 git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 npm ci
 npm run deploy
