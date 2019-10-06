45e269a274cc9902d4332402f01276cc98ef0b4f
Ann Cudis <ann.cudis@xero.com>
2019-10-06 18:54:29 +1300

    Do not use gh-pages in deploying

--- actions/build-deploy/entrypoint.sh
+++ actions/build-deploy/entrypoint.sh
@@ -3,4 +3,17 @@
 export CI=true
 
 npm ci
-npm run deploy
+npm run pre-deploy
+
+cd build
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+remote_branch="gh-pages" && \
+git init && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git add . && \
+echo -n 'Files to Commit:' && ls -l | wc -l && \
+git commit -m 'action build' > /dev/null 2>&1 && \
+git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
+rm -fr .git && \
+cd ../
