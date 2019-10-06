78206895dfc9b63f9ad0fb10968177afa53e140b
Ann Cudis <ann.cudis@xero.com>
2019-10-06 19:30:58 +1300

    Install gh-pages globally

--- actions/build-deploy/entrypoint.sh
+++ actions/build-deploy/entrypoint.sh
@@ -3,7 +3,7 @@
 npm ci
 npm run predeploy
 
-npm install gh-pages && \
+npm install gh-pages --global && \
 gh-pages -r "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" \
          -b "gh-pages2" \
          -m "Update page content" \
